150acbad762d070f2fa7b94f127881fceb2ff30b
laalaguer <laalaguer@gmail.com>
2018-02-26 14:33:39 +0800

    1 worker in production is enough

--- prod.run.sh
+++ prod.run.sh
@@ -1,5 +1,5 @@
 # Runner in Production
-gunicorn prod:my_app -b 127.0.0.1:8000 -D --workers 3 --reload
+gunicorn prod:my_app -b 127.0.0.1:8000 -D --workers 1 --reload
 echo "gunicorn in background"
 #rm -f /etc/nginx/sites-enabled/default
 nginx -g "daemon off;"
